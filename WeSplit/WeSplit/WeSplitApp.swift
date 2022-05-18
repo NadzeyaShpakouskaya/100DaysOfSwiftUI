cbce53055c40305b10b7757b975646488805085e
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2022-05-18 10:14:54 +0400

    Project #1. We split

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nadzeya Shpakouskaya on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

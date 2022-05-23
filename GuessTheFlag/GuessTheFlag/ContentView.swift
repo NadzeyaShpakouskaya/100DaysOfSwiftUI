f77f58951feccdf8285716faaaf7f15e3a328e42
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2022-05-23 13:28:52 +0400

    # Project #2.  Finished project. Add score tracking Restart game functionality Limit number of questions

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var gameEnded = false
     @State private var scoreTitle = ""
-    
+    @State private var score = 0
+    @State private var questionCounter = 0
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -58,7 +60,7 @@ struct ContentView: View {
                     Spacer()
                 }
                 Spacer()
-                Text("Score: ???")
+                Text("Score: \(score)")
                     .font(.largeTitle.bold())
                     .foregroundColor(.white)
                 Spacer()
@@ -66,20 +68,40 @@ struct ContentView: View {
             .padding()
         }.alert (scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
+        }
+        .alert("Game is over.", isPresented: $gameEnded) {
+            Button("Restart game", action: restartGame)
         } message: {
-            Text("Your score is ????")
+            Text("Your score is \(score)")
         }
     }
     
     private func flagTapped(_ number: Int) {
-        scoreTitle = correctAnswer == number ? "Correct" : "Wrong"
+        if correctAnswer == number {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong. That's the flag of \(countries[number])"
+        }
         showingScore = true
     }
     
     private func askQuestion() {
+        if questionCounter < 7 {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+            questionCounter += 1
+        } else {
+            gameEnded = true
+        }
     }
+    
+    private func restartGame() {
+        score = 0
+        questionCounter = 0
+        gameEnded = false
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

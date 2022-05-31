479b3acebd2699931b266222997533191e534074
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2022-05-31 10:03:35 +0400

    project 4.  Better sleep

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,18 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nadzeya Shpakouskaya on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            FormView()
+        }
+    }
+}

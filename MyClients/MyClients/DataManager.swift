8aa9ddf2a6909c4e2a96a6af438a628e43a21aba
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2022-06-29 08:36:07 +0400

    bug fix DataManager - ManagedObjectContext

--- MyClients/MyClients/DataManager.swift
+++ MyClients/MyClients/DataManager.swift
@@ -8,33 +8,33 @@
 import CoreData
 import SwiftUI
 
-class DataManager: ObservableObject {
+@MainActor class DataManager: ObservableObject {
     let networkManager: NetworkDataManager  = NetworkDataManager.shared
-    @Environment(\.managedObjectContext) var moc
-    @Published var dataLoaded = false
+    let localManager: LocalDataManager = LocalDataManager.shared
+    
+    @Published var dataLoadedAndCached = false
+    var dataLoaded = false
+    
     
     init()  {}
     
     // fetch data from network
-    @MainActor  func fetchUserList() async  -> [User] {
+    func fetchUserList() async  -> [User] {
         let data = await networkManager.fetchUserList()
         if !data.isEmpty {
             dataLoaded = true
         }
         return data
     }
-      
-    // TO DO - Need to fix, can't use this method
-    // get an error -
-    // nilError: The operation couldn’t be completed. (Foundation._GenericObjCError error 0.)
-    @MainActor func saveToCoreData() async {
+
+    func saveToCoreData() async {
         if !dataLoaded {
             let data = await fetchUserList()
             
             if !data.isEmpty {
                 // transform data to CoreData Entities
                 for user in data {
-                    let cachedUser = CachedUser(context: moc)
+                    let cachedUser = CachedUser(context: localManager.container.viewContext)
                     cachedUser.id = user.id
                     cachedUser.name = user.name
                     cachedUser.address = user.address
@@ -46,21 +46,17 @@ class DataManager: ObservableObject {
                     cachedUser.company = user.company
                     cachedUser.isActive = user.isActive
                     for friend in user.friends {
-                        let cachedFriend  = CachedFriend(context: moc)
+                        let cachedFriend  = CachedFriend(context: localManager.container.viewContext)
                         cachedFriend.id = friend.id
                         cachedFriend.name = friend.name
                         cachedUser.addToFriends(cachedFriend)
                     }
                 }
             }
-            if moc.hasChanges {
-                do {
-                    try moc.save()
-                } catch {
-                    print("\(error): \(error.localizedDescription)")
-                }
-                print("caching ended...")
-            }
+            
+            localManager.saveContext()
+            dataLoadedAndCached = true
+
         }
     }
     

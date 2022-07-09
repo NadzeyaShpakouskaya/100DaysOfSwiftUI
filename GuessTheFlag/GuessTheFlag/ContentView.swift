3e48a80f3e0e0a306c807f6530ca1a38c7c064a1
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2022-07-09 08:59:15 +0400

    add accessibility for VoiceOver

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -19,6 +19,20 @@ struct ContentView: View {
     @State private var isFlagTapped = false
     @State private var selectedAnswer = -1
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     var QuestionView: some View {
         
         VStack (spacing: 15) {
@@ -39,6 +53,7 @@ struct ContentView: View {
 
                 } label: {
                     FlagView(countryImage: countries[number])
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                 }
                 .rotation3DEffect(number == selectedAnswer ? .degrees(360) : .degrees(0), axis: (x: 0, y: 1, z: 0))
                 
@@ -135,6 +150,7 @@ struct FlagView: View {
             .renderingMode(.original)
             .clipShape(Capsule())
             .shadow(color: .gray, radius: 10, x: -5, y: 5)
+        
     }
 }
 

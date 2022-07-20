0e796bc5596054e942e9f5e5133e3ccccff89a66
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2022-07-20 16:03:56 +0400

    - add context image for QR-code ( to save it to the library), - add local notifications + one more swipe action for it, - save data to UserDefaults.

---
+++ Hot Prospects/Hot Prospects/Prospect.swift
@@ -0,0 +1,50 @@
+//
+//  Prospect.swift
+//  Hot Prospects
+//
+//  Created by Nadzeya Shpakouskaya on 19/07/2022.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published private(set) var people: [Prospect]
+    
+    let saveKey = "SavedData"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+
+    
+    func toggle(_ prospect: Prospect) {
+        // we should use it before changes
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+}

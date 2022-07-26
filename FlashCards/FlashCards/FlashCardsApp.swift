4c319635882e75ae8cae4eea2d4e33a5668c82cf
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2022-07-26 16:40:42 +0400

    Theoretical examples

---
+++ FlashCards/FlashCards/FlashCardsApp.swift
@@ -0,0 +1,18 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Nadzeya Shpakouskaya on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            BasicTapGestureView()
+            TimerTriggeredView()
+        }
+    }
+}
